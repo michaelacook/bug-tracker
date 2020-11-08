d2777f3e42209066a2a2dc254bc0cff0927a8e11
Michael A. Cook <mcook0775@gmail.com>
2020-11-08 12:56:20 -0500

    Add allProjectsGET

--- controllers/ProjectController.js
+++ controllers/ProjectController.js
@@ -1,1 +1,23 @@
-module.exports = {}
+const ProjectService = require("../services/ProjectService")
+
+module.exports = {
+  /**
+   * Handle route /projects/all GET
+   * @param {Object} req - HTTP request object 
+   * @param {Object} res - HTTP response object 
+   * @param {Function} next - next middleware
+   * @return {Object} res 
+   * @return {Function} next
+   */
+  allProjectsGET: async (req, res, next) => {
+    try {
+      const users = req.query.users
+      const projects = await ProjectService.getAllProjects(
+        users ? true : null
+      )
+      return res.status(200).json(projects)
+    } catch (err) {
+      next(err)
+    }
+  }
+}
