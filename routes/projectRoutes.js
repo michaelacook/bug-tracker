9ca531ac7d5cf52d82a26a0c188e8364722a19db
Michael A. Cook <mcook0775@gmail.com>
2020-11-08 13:00:50 -0500

    Add route for /projects/all

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -4,4 +4,8 @@ const express = require("express")
 const router = express.Router()
 const ProjectController = require("../controllers/ProjectController")
 
+router.get("/all", (req, res, next) => 
+  ProjectController.allProjectsGET(req, res, next)
+)
+
 module.exports = router
