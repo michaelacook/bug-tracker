8fb45703681e13b98e094493741a31ede6e9bb32
Michael A. Cook <mcook0775@gmail.com>
2020-11-08 17:50:04 -0500

    Add core methods

--- services/IssueService.js
+++ services/IssueService.js
@@ -1,1 +1,45 @@
-module.exports = {}
+const { Issue, IssueHistory, Project } = require("../models/index")
+
+module.exports = {
+  getAllIssues: async (comments = true) => {
+    try {
+    } catch (err) {
+      Promise.reject(err)
+    }
+  },
+
+  getOneIssue: async (id, comments = true) => {
+    try {
+    } catch (err) {
+      Promise.reject(err)
+    }
+  },
+
+  getIssuesByProject: async (projectId) => {
+    try {
+    } catch (err) {
+      Promise.reject(err)
+    }
+  },
+
+  addIssue: async () => {
+    try {
+    } catch (err) {
+      Promise.reject(err)
+    }
+  },
+
+  updateIssue: async (id) => {
+    try {
+    } catch (err) {
+      Promise.reject(err)
+    }
+  },
+
+  deleteIssue: async (id) => {
+    try {
+    } catch (err) {
+      Promise.reject(err)
+    }
+  },
+}
