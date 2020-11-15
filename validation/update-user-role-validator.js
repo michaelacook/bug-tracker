bc648920d603c6ec5218d3764d04cb91009dc4a0
Michael A. Cook <mcook0775@gmail.com>
2020-11-15 14:00:15 -0500

    Fix

--- validation/update-user-role-validator.js
+++ validation/update-user-role-validator.js
@@ -4,7 +4,12 @@ module.exports = [
   body("roleId")
     .exists()
     .isNumeric()
-    .withMessage("Role id must be an integer between 1 and 3.")
+    .withMessage("Role id must be an integer between 1 and 5.")
+    .isInt({
+      min: 1,
+      max: 5,
+    })
+    .withMessage("role id must fall between 1 and 5.")
     .isLength({
       min: 1,
       max: 1,
