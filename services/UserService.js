c4ff5f2dc26fd22faf61c3e8f740b8b3eb262c13
Michael A. Cook <mcook0775@gmail.com>
2020-11-15 14:01:26 -0500

    Remove validation from updateUserRole, handle in validation middleware instead

--- services/UserService.js
+++ services/UserService.js
@@ -119,8 +119,6 @@ module.exports = {
    */
   updateUserRole: async (id, roleId) => {
     try {
-      if (roleId < 2 || roleId > 5)
-        throw new Error("Role id falls outside acceptable range.")
       await User.sync()
       await User.update(
         {
