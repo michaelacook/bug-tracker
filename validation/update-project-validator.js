fd0d71920903160511d6f8d38e5fd4513a927033
Michael A. Cook <mcook0775@gmail.com>
2020-11-19 17:18:42 -0500

    Remove custom messages

--- validation/update-project-validator.js
+++ validation/update-project-validator.js
@@ -1,15 +1,7 @@
 const { body } = require("express-validator")
 
 module.exports = [
-  body("title")
-    .optional()
-    .withMessage("Please supply a title for the project.")
-    .isAlpha()
-    .withMessage("Title must contain only letters"),
-  body("description")
-    .optional()
-    .withMessage("Please supply a project description."),
-  body("projectManager")
-    .isInt()
-    .withMessage("Project manager id must be an integer."),
+  body("title").optional().isAlpha(),
+  body("description").optional(),
+  body("projectManager").isInt(),
 ]
